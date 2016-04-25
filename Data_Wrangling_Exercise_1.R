32ca9eb070904bce5508794197a6d63024c8e062
jdubson <jasonhsuwu@gmail.com>
2016-04-24 22:35:32 -0700

    Add product categories

--- Data_Wrangling_Exercise_1.R
+++ Data_Wrangling_Exercise_1.R
@@ -1,11 +1,21 @@
 library(tidyr)
 library(dplyr)
 
-# Load the data
+# 0. Load the data
 refine_original <- read.csv("~/Desktop/Springboard/refine_original.csv", header=TRUE, sep=",")
 
-# Clean up brand names
+# 1. Clean up brand names
 refine_original[[1]] = tolower(refine_original[[1]])
 
-# Separate product code and number
-separate(refine_original, Product.code...number, c("product_code","product_number"), sep="-")
+# 2. Separate product code and number
+refine_original <- separate(refine_original, Product.code...number, c("product_code","product_number"), sep="-")
+
+# 3. Add product categories
+
+# Create dataframe matching code with category
+product_code = c("p","v","x","q")
+category = c("Smartphone","TV","Laptop","Tablet")
+df = data.frame(product_code, category)
+
+# Join df to refine_original to add categories
+refine_original <- left_join(refine_original, df, by="product_code")
