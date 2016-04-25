406b210892d30caefd0416e88411372ea883ab2f
jdubson <jasonhsuwu@gmail.com>
2016-04-24 23:35:13 -0700

    Create 'refine_clean' data frame. Add full address for geocoding.

--- Data_Wrangling_Exercise_1.R
+++ Data_Wrangling_Exercise_1.R
@@ -3,12 +3,13 @@ library(dplyr)
 
 # 0. Load the data
 refine_original <- read.csv("~/Desktop/Springboard/refine_original.csv", header=TRUE, sep=",")
+refine_clean <- refine_original
 
 # 1. Clean up brand names
-refine_original[[1]] = tolower(refine_original[[1]])
+refine_clean$company <- tolower(refine_clean$company)
 
 # 2. Separate product code and number
-refine_original <- separate(refine_original, Product.code...number, c("product_code","product_number"), sep="-")
+refine_clean <- separate(refine_clean, Product.code...number, c("product_code","product_number"), sep="-")
 
 # 3. Add product categories
 
@@ -18,4 +19,7 @@ category = c("Smartphone","TV","Laptop","Tablet")
 df = data.frame(product_code, category)
 
 # Join df to refine_original to add categories
-refine_original <- left_join(refine_original, df, by="product_code")
+refine_clean <- left_join(refine_clean, df, by="product_code")
+
+# 4. Add full address for geocoding
+refine_clean <- mutate(refine_clean, full_address = paste(address, city, country, sep = ", "))
