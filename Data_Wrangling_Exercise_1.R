881bd862fd96a6a3b4902bab9b91c88e73fb88c7
jdubson <jasonhsuwu@gmail.com>
2016-05-04 12:24:27 -0700

    Fix brand name misspellings

--- Data_Wrangling_Exercise_1.R
+++ Data_Wrangling_Exercise_1.R
@@ -7,6 +7,9 @@ refine_clean <- refine_original
 
 # 1. Clean up brand names
 refine_clean$company <- tolower(refine_clean$company)
+refine_clean$company <- gsub("(ph|f)(i|l){2,4}ps", "philips", refine_clean$company)
+refine_clean$company <- gsub("ak\\s{0,1}z(0|o)", "akzo", refine_clean$company)
+refine_clean$company <- gsub("unilver", "unilever", refine_clean$company)
 
 # 2. Separate product code and number
 refine_clean <- separate(refine_clean, Product.code...number, c("product_code","product_number"), sep="-")
@@ -23,3 +26,5 @@ refine_clean <- left_join(refine_clean, df, by="product_code")
 
 # 4. Add full address for geocoding
 refine_clean <- mutate(refine_clean, full_address = paste(address, city, country, sep = ", "))
+
+# 5. Create dummy variables for company and product category
