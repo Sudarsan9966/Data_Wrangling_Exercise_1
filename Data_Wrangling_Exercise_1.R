cf34a07d2a1ec71f0a04f00518aa9948162b4dc6
jdubson <jasonhsuwu@gmail.com>
2016-05-04 14:16:28 -0700

    Use piping to clean up code. Use mutate() instead of left_join() to add product categories.

--- Data_Wrangling_Exercise_1.R
+++ Data_Wrangling_Exercise_1.R
@@ -12,19 +12,14 @@ refine_clean$company <- gsub("ak\\s{0,1}z(0|o)", "akzo", refine_clean$company)
 refine_clean$company <- gsub("unilver", "unilever", refine_clean$company)
 
 # 2. Separate product code and number
-refine_clean <- separate(refine_clean, Product.code...number, c("product_code","product_number"), sep="-")
-
 # 3. Add product categories
-
-# Create dataframe matching code with category
-product_code = c("p","v","x","q")
-category = c("Smartphone","TV","Laptop","Tablet")
-df = data.frame(product_code, category)
-
-# Join df to refine_original to add categories
-refine_clean <- left_join(refine_clean, df, by="product_code")
-
 # 4. Add full address for geocoding
-refine_clean <- mutate(refine_clean, full_address = paste(address, city, country, sep = ", "))
+refine_clean <- refine_clean %>% 
+  separate(Product.code...number, c("product_code","product_number"), sep="-") %>% 
+  mutate(product_category = ifelse(product_code %in% "p", "Smartphone", 
+                                   ifelse(product_code %in% "v", "TV", 
+                                          ifelse(product_code %in% "x", "Laptop", 
+                                                 ifelse(product_code %in% "q", "Tablet", "Other"))))) %>% 
+  mutate(full_address = paste(address, city, country, sep = ", "))
 
 # 5. Create dummy variables for company and product category
