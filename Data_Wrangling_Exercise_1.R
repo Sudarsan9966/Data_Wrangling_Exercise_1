146afd4135a8f24173db3471b4a347e10fd1526c
jdubson <jasonhsuwu@gmail.com>
2016-05-04 14:46:10 -0700

    Created dummy variables for company and category products.

--- Data_Wrangling_Exercise_1.R
+++ Data_Wrangling_Exercise_1.R
@@ -14,12 +14,20 @@ refine_clean$company <- gsub("unilver", "unilever", refine_clean$company)
 # 2. Separate product code and number
 # 3. Add product categories
 # 4. Add full address for geocoding
+# 5. Create dummy variables for company and product category
 refine_clean <- refine_clean %>% 
   separate(Product.code...number, c("product_code","product_number"), sep="-") %>% 
   mutate(product_category = ifelse(product_code %in% "p", "Smartphone", 
                                    ifelse(product_code %in% "v", "TV", 
                                           ifelse(product_code %in% "x", "Laptop", 
                                                  ifelse(product_code %in% "q", "Tablet", "Other"))))) %>% 
-  mutate(full_address = paste(address, city, country, sep = ", "))
+  mutate(full_address = paste(address, city, country, sep = ", ")) %>%
+  mutate(company_philips = ifelse(company %in% "philips", 1, 0)) %>%
+  mutate(company_akzo = ifelse(company %in% "akzo", 1, 0)) %>%
+  mutate(company_van_houten = ifelse(company %in% "van houten", 1, 0)) %>%
+  mutate(company_unilever = ifelse(company %in% "unilever", 1, 0)) %>%
+  mutate(product_smartphone = ifelse(product_category %in% "Smartphone", 1, 0)) %>%
+  mutate(product_tv = ifelse(product_category %in% "TV", 1, 0)) %>%
+  mutate(product_laptop = ifelse(product_category %in% "Laptop", 1, 0)) %>%
+  mutate(product_tablet = ifelse(product_category %in% "Tablet", 1, 0))
 
-# 5. Create dummy variables for company and product category
